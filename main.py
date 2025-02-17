0b9ea1e30ac64aa6d48b44979118f14a0a4364ab
Pashtet <pavel.pivikov2008@yandex.ru>
2025-02-17 21:57:06 +0300

    Espresso

---
+++ main.py
@@ -0,0 +1,46 @@
+import sys
+from PyQt6 import uic
+import sqlite3
+from PyQt6.QtWidgets import QApplication, QMainWindow
+
+
+class MyWidget(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.connection = sqlite3.connect('coffee.sqlite')
+        self.cursor = self.connection.cursor()
+        uic.loadUi('Main.ui', self)
+        self.pushButton.clicked.connect(self.render)
+
+    def render(self):
+        name_coffee = self.lineEdit.text()
+        result = self.cursor.execute(f"""SELECT id FROM Data WHERE Sort = '{name_coffee}'""").fetchone()
+        if result is not None:
+            self.label.setText(f'ID: {result[0]}')
+            self.label_2.setText('Сорт: ' + self.cursor.execute(f"""SELECT Sort FROM
+             Data WHERE id = '{result[0]}'""").fetchone()[0])
+            self.label_3.setText('Степень обжарки: ' + self.cursor.execute(f"""SELECT Degree_roasting FROM
+             Data WHERE id = '{result[0]}'""").fetchone()[0])
+            self.label_4.setText('Вид: ' + self.cursor.execute(f"""SELECT Viev FROM
+             Data WHERE id = '{result[0]}'""").fetchone()[0])
+            self.label_5.setText('Описание вкуса: ' + self.cursor.execute(f"""SELECT Taste_description FROM
+             Data WHERE id = '{result[0]}'""").fetchone()[0])
+            self.label_6.setText('Цена: ' + self.cursor.execute(f"""SELECT Price FROM
+             Data WHERE id = '{result[0]}'""").fetchone()[0])
+            self.label_7.setText('Объём упаковки: ' + self.cursor.execute(f"""SELECT Volume FROM
+             Data WHERE id = '{result[0]}'""").fetchone()[0])
+        else:
+            self.label.setText('ID: Данный сорт кофе не найден в базе данных')
+            self.label_2.setText('')
+            self.label_3.setText('')
+            self.label_4.setText('')
+            self.label_5.setText('')
+            self.label_6.setText('')
+            self.label_7.setText('')
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    ex = MyWidget()
+    ex.show()
+    sys.exit(app.exec())
