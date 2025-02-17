00a75a71bfec737713c48ba4b47a1338499b5b67
Pashtet <pavel.pivikov2008@yandex.ru>
2025-02-17 20:14:37 +0300

    Yellow circles

---
+++ 45.py
@@ -0,0 +1,42 @@
+import sys
+
+from PyQt6 import uic
+from PyQt6.QtGui import QPainter, QPen, QColor
+from PyQt6.QtCore import QRectF
+from random import randint, choice
+from PyQt6.QtWidgets import QApplication, QMainWindow
+
+
+class MyWidget(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        uic.loadUi('Ui.ui', self)
+        self.pushButton.clicked.connect(self.paint)
+        self.do_paint = False
+
+    def paintEvent(self, event):
+        if self.do_paint:
+            qp = QPainter()
+            qp.begin(self)
+            self.draw_circle(qp)
+            qp.end()
+        self.do_paint = False
+
+    def paint(self):
+        self.do_paint = True
+        self.update()
+
+    def draw_circle(self, qp):
+        qp.setBrush(QColor(255, 255, 0))
+        x = randint(0, 745)
+        y = randint(0, 450)
+        r = randint(0, 100)
+        rectangle = QRectF(x, y, r, r)
+        qp.drawEllipse(rectangle)
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    ex = MyWidget()
+    ex.show()
+    sys.exit(app.exec())
